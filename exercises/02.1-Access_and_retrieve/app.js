d7de06c5f4bb92fd863d930cac725b7960284f1d
Alejandro <alejandrorcc24@gmail.com>
2025-02-21 18:33:43 +0000

    listo

--- exercises/02.1-Access_and_retrieve/app.js
+++ exercises/02.1-Access_and_retrieve/app.js
@@ -1,8 +1,14 @@
 // Declaring the array
+
+//. positions:    0        1         2          3          4         5         6
 let myArray = ['sunday','monday','tuesday','wednesday','thursday','friday','saturday'];
 
-// 1. print the 3rd item here
 
-// 2. change the 'thursday' value to null here
+// 1. print the 3rd item here
+console.log(myArray[2]);
+
+// 2. change 'thursday' value to null here
+myArray[4] = null;
 
 // 3. print the position of step 2
+console.log(myArray[4]);
